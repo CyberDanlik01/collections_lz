9a3b44d271f54d79381933e4bd1178e5cc3548bf
CyberDanlik01 <gorillaa@tutamail.com>
2025-10-27 17:52:13 +0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,16 +1,19 @@
 import matplotlib.pyplot as plt
+
 #Моя функция: y = (x**4) + 6(x**2) + 15
 x = []
 y = []
+
 # все x
 for i in range(25):
     x.append(i)
+
 #Найдем к ним y
 for i in range(len(x)):
     y.append((x[i] ** 4) + 6 * (x[i] ** 2) + 15)
 
 plt.plot(x, y) #График
-plt.xlabel('Ось х') #Подпись для оси Ox
-plt.ylabel('Ось y') #Подпись для оси Oy
+plt.xlabel('Ось х') #Подпись для оси x
+plt.ylabel('Ось y') #Подпись для оси y
 plt.title('График') #Название
 plt.show() #Вывод графика на экран
